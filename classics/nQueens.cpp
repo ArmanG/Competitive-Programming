ed29a28bb94098c4e0c2fa460667e84a6562afd0
Arman Ghassemi <arman.gh45@gmail.com>
2015-06-23 14:49:21 -0400

    add classic problem nQueens

---
+++ classics/nQueens.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int boardSize;
+int total;
+vector<int> arrangement;
+
+bool isSafeSpot( int column, int k ) {
+
+	for ( int i=0; i<k; i++ ) {
+		if ( arrangement[i]==column || abs( arrangement[i]-column )==abs( i-k ) ) {
+			return false;
+		}
+	}
+
+	return true;
+
+}
+
+void placeQueens( int k ) {
+
+	if ( k == boardSize ) {
+		total++;
+		return;
+	} 
+
+	for ( int i=0; i<boardSize; i++ ) {
+		if ( isSafeSpot( i, k ) ) {
+			arrangement[k]=i;
+			placeQueens( k+1 );
+			arrangement[k]=-1;
+		}
+	}
+
+}
+
+
+int main () {
+
+	cin >> boardSize;
+	arrangement.resize(baordSize);
+	placeQueens(0);
+	cout << total << endl;	
+
+	return 0;
+
+}
