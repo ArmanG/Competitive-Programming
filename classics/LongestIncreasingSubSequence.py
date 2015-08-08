9806a343fc5c231921aa12901c0ccd6ba115c400
Arman Ghassemi <arman.gh45@gmail.com>
2015-08-08 17:39:04 -0400

    Update LongestIncreasingSubSequence.py

--- classics/LongestIncreasingSubSequence.py
+++ classics/LongestIncreasingSubSequence.py
@@ -7,14 +7,11 @@ n=0
 def LIS():
 	for i in range(n):
 		optimal=0
-
 		for j in range(i-1, -1, -1):
 			if seq[j] < seq[i] and best[j] > optimal:
 				optimal=best[j]
 			if j <= optimal: break
-
 		best[i]=optimal+1
-
 	return max(best)
 
 allData = map( int, sys.stdin.read().split() )
